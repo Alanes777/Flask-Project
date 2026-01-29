b1fde362f6ed1a4cb491539b6f40c3c4a163738c
Alanes Brasil <alanesbrasil26@gmail.com>
2026-01-29 15:26:14 -0300

    Corrige rota home

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint 
+from flask import Blueprint, render_template
 
 views = Blueprint('views')
 
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1"
+    return render_template("home.html")
+    
+    If ___name___ == "___main___" 
+    Debug=true
